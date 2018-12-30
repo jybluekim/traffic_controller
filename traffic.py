21cb74fe76d669dcf438791933b9dad7d1a9a3c1
Joon Kim <jkim@nvxltech.com>
2018-12-29 23:42:13 -0800

    Added feature to cycle thru colors

--- traffic.py
+++ traffic.py
@@ -1,9 +1,34 @@
 class controller:
-    def __init__(self):
+    def __init__(self, priority=False):
         self.state = "RESET"
+        self.priority = priority 
+
+    def tick(self, waiting_mine=False, waiting_opp=False):
+        if self.state == "RESET":
+            if self.priority == True:
+                self.state = "GREEN"
+            else:
+                self.state = "RED"
+        elif self.state == "RED":
+            self.state = "GREEN"
+        elif self.state == "GREEN":
+            self.state = "YELLOW"
+        elif self.state == "YELLOW":
+            self.state = "RED"
+
+
+    def output(self):
+        if self.state == "RESET" or self.state == "RED":
+            return "RED"
+        elif self.state == "YELLOW" or self.state == "GREEN":
+            return self.state
+
 
 
 if __name__ == "__main__":
-    ctrl = controller()
+    ctrl = controller(priority=True)
 
-    
+    for t in range(10):
+        ctrl.tick()
+        print (ctrl.output())
+
